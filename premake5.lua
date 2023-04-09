ee7f556511876d7f34a014ce114fbd328c4608ea
Fernando Lobaina <fernando.lobaina512@gmail.com>
2023-04-09 05:51:49 -0400

    Added missing files to premake.

--- premake5.lua
+++ premake5.lua
@@ -8,16 +8,20 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
 		"imgui_internal.h",
-		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+
 	}
+	
+
 
 	filter "system:windows"
 		systemversion "latest"
@@ -25,10 +29,10 @@ project "ImGui"
 		staticruntime "On"
 
 	filter "system:linux"
-		pic "On"
+		pic "on"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
+		staticruntime "on"
 
 	filter "configurations:Debug"
 		runtime "Debug"
